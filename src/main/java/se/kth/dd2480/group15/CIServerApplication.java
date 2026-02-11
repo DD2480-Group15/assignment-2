105e397a62fbc7e57387a30c930b0279af926041
RasmusSjo <rasmus.sjoberg15@gmail.com>
2026-02-11 02:17:23 +0100

    chore: add config files for build storage, refs #33

--- src/main/java/se/kth/dd2480/group15/CIServerApplication.java
+++ src/main/java/se/kth/dd2480/group15/CIServerApplication.java
@@ -1,12 +1,14 @@
 package se.kth.dd2480.group15;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import se.kth.dd2480.group15.infrastructure.config.StorageProperties;
 
-
+@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 public class CIServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CIServerApplication.class, args);
     }
-
 }
