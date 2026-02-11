28e7a6cd212c7c7d08574d0913357ccb6c0ebef0
filippaco <filippacolsson@gmail.com>
2026-02-11 17:57:35 +0100

    fix: put build-details script logic into correct function

--- src/main/resources/static/build-details.js
+++ src/main/resources/static/build-details.js
@@ -1,29 +1,33 @@
-const urlParams = new URLSearchParams(window.location.search);
-const buildId = urlParams.get('id');
+async function loadBuildDetails(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const buildId = urlParams.get('id');
 
-if (!buildId) {
-        console.error("No buildID found");
-        return;
+    if (!buildId) {
+            console.error("No buildID found");
+            return;
+        }
+
+    try{
+        const metaResponse = await fetch(`/api/v1/builds/${buildId}`);
+        const metaData = await metaResponse.json();
+
+        const logResponse = await fetch(`/api/v1/builds/${buildId}/log`);
+        const logData = await logResponse.json();
+
+        displayData(metaData, logData);
+    } 
+    catch(error){
+        console.error("Communication error", error);
     }
 
-try{
-    const metaResponse = await fetch(`/api/v1/builds/${buildId}`);
-    const metaData = await metaResponse.json();
-
-    const logResponse = await fetch(`/api/v1/builds/${buildId}/log`);
-    const logData = await logResponse.json();
-
-    displayData(metaData, logData);
-} 
-catch(error){
-    console.error("Communication error", error);
+    function displayData(meta, logBody){
+        document.getElementById('header-id').innerText = `Build #${meta.buildId}`;
+        document.getElementById('build-status').innerText = meta.status;
+        document.getElementById('build-commit').innerText = meta.commitSha;
+        document.getElementById('build-author').innerText = meta.owner;
+        document.getElementById('build-date').innerText = meta.createdAt;
+        document.getElementById('log-output').innerText = logBody.log;
+    }
 }
 
-function displayData(meta, logBody){
-    document.getElementById('header-id').innerText = `Build #${meta.buildId}`;
-    document.getElementById('build-status').innerText = meta.status;
-    document.getElementById('build-commit').innerText = meta.commitSha;
-    document.getElementById('build-author').innerText = meta.owner;
-    document.getElementById('build-date').innerText = meta.createdAt;
-    document.getElementById('log-output').innerText = logBody.log;
-}
+loadBuildDetails()
